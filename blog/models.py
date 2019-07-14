907761ed5f586227134d457b7d8a41501e5c7ac6
Federico <federicocipri@gmail.com>
2019-07-14 23:30:54 +0100

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,15 @@ from django.db import models
 class Blog(models.Model):
 
 	title = models.CharField(max_length=100)
-	pub_date = models.DateField(auto_now=True)
+	pub_date = models.DateTimeField(auto_now=False)
 	text = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.text[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
