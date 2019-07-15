ec75d472b697e7f03615f320ae696d15f250c01f
Federico <federicocipri@gmail.com>
2019-07-15 22:45:06 +0100

    Last updates preupload

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'mj&W5*v*',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
